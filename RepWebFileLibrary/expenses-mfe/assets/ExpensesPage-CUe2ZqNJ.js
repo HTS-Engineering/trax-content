773b5a2e487aaa235acf684ca622a1a7c8f51d92
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2025-09-12 13:21:36 -0400

    feat(expensesApp): #TRX-68 Expenses type table. 7

---
+++ RepWebFileLibrary/expenses-mfe/assets/ExpensesPage-CUe2ZqNJ.js
@@ -0,0 +1,8 @@
+import { j as jsxRuntimeExports } from "./__federation_expose_Mount-BlmaWVoB.js";
+import { E as ExpensesList } from "./ExpensesList-2zKP_fwt.js";
+const ExpensesPage = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(ExpensesList, {});
+};
+export {
+  ExpensesPage as default
+};
