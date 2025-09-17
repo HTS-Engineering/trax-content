0ccdfb4a488718fa33f17f7cbcb9d5c4ba1326c0
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2025-09-16 22:23:49 -0400

    feat(expenses): #TRX-13 Handle authentication session across microfrontend and Base System. Prevent Shell app reloading

---
+++ RepWebFileLibrary/expenses-mfe/assets/axiosStrategy-lDeKH0HY.js
@@ -0,0 +1,22 @@
+import { i as apiClient } from "./__federation_expose_Mount-D5bfMARr.js";
+class AxiosStrategy {
+  active = false;
+  async initialize() {
+    if (this.active) {
+      console.warn("Axios interceptor is already active");
+      return;
+    }
+    this.active = true;
+  }
+  async stop() {
+    if (!this.active) return;
+    apiClient.cleanup();
+    this.active = false;
+  }
+  isActive() {
+    return this.active;
+  }
+}
+export {
+  AxiosStrategy
+};
