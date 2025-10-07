deca77fc5d81a52afd397850f0a114b199c95d49
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2025-10-07 13:18:14 -0400

    chore(expenses): Test build

--- RepWebFileLibrary/expenses-mfe/assets/remoteEntry.js
+++ RepWebFileLibrary/expenses-mfe/assets/remoteEntry.js
@@ -3,7 +3,7 @@ const exportSet = /* @__PURE__ */ new Set(["Module", "__esModule", "default", "_
 let moduleMap = {
   "./mount": () => {
     dynamicLoadingCss(["style-DD1nNig8.css"], false, "./mount");
-    return __federation_import("./assets/__federation_expose_Mount-B34jWEhU.js").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
+    return __federation_import("./assets/__federation_expose_Mount-DHa2-_91.js").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
   }
 };
 const seen = {};
