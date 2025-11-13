f73f76325efdc2f8eb908b1156ea2b4f4cf4f5e9
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2025-11-13 10:46:51 -0500

    feat(expensesApp): #TRX-133, #TRX-133 Cost allocation. Models adjustment with new BE contracts

--- RepWebFileLibrary/expenses-mfe-dev/assets/ExpensesPage-VjOnCwM7.js
+++
@@ -1,8 +0,0 @@
-import { j as jsxRuntimeExports } from "./jsx-runtime-DLKWXVrv.js";
-import { E as ExpensesList } from "./ExpensesList-pc4VtIdV.js";
-const ExpensesPage = () => {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx(ExpensesList, {});
-};
-export {
-  ExpensesPage as default
-};
