6ebd49ea3d5bc644aff0c7f71d750ba7446e3d4f
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2025-12-16 10:34:29 -0500

    feat(expenses): #TRX-188 Expenses list default sorting, clear search by id when filter changes. New SSL MFE 3

--- RepWebFileLibrary/expenses-mfe-dev/assets/remoteEntry.js
+++ RepWebFileLibrary/expenses-mfe-dev/assets/remoteEntry.js
@@ -3,7 +3,7 @@ const exportSet = /* @__PURE__ */ new Set(["Module", "__esModule", "default", "_
 let moduleMap = {
   "./mount": () => {
     dynamicLoadingCss(["style-UAjhty0E.css"], false, "./mount");
-    return __federation_import("./assets/__federation_expose_Mount-BCc-_0s-.js").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
+    return __federation_import("./assets/__federation_expose_Mount-xz31GyXT.js").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
   }
 };
 const seen = {};
---
+++ RepWebFileLibrary/expenses-mfe-ssl-test/assets/ExpensesPage-Boc8zeGK.js
@@ -0,0 +1,8 @@
+import { j as jsxRuntimeExports } from "./jsx-runtime-CzdF90_e.js";
+import { E as ExpensesList } from "./ExpensesList-x0g7QI9w.js";
+const ExpensesPage = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(ExpensesList, {});
+};
+export {
+  ExpensesPage as default
+};
