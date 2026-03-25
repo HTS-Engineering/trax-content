2e30cc0a28b6333e3ebad4dfebe0843542c21bed
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2026-03-25 16:39:36 -0400

    feat(expensesMfeDevTest): #TRX-245 Add approve action for approver expense detail

---
+++ RepWebFileLibrary/expenses-mfe-ssl-test/assets/ExpensesPage-CSu4R0yz.js
@@ -0,0 +1,10 @@
+import { j as jsxRuntimeExports } from "./jsx-runtime-TULtkvNU.js";
+import { E as ExpensesList } from "./ExpensesList-DPpUWIKG.js";
+import "./axiosInstance-BTNM_CBw.js";
+import "./expense-api-BRDfxHyw.js";
+const ExpensesPage = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(ExpensesList, {});
+};
+export {
+  ExpensesPage as default
+};
--- RepWebFileLibrary/expenses-mfe-ssl-test/assets/remoteEntry.js
+++ RepWebFileLibrary/expenses-mfe-ssl-test/assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
 const exportSet = /* @__PURE__ */ new Set(["Module", "__esModule", "default", "_export_sfc"]);
 let moduleMap = {
   "./mount": () => {
-    dynamicLoadingCss(["style-BMPZjHsy.css"], false, "./mount");
-    return __federation_import("./assets/__federation_expose_Mount-BaQMzd2D.js").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
+    dynamicLoadingCss(["style-Ccxj5up1.css"], false, "./mount");
+    return __federation_import("./assets/__federation_expose_Mount-2Qj3SJLq.js").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
   }
 };
 const seen = {};
