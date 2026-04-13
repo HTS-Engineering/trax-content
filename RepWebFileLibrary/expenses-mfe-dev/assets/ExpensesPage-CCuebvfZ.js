0151189623de53146485842c9686a8472e29a09b
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2026-04-13 16:01:34 -0400

    feat(ExpensesDevSit): #TRX-518, #TRX-542, #TRX-533, #TRX-546

---
+++ RepWebFileLibrary/expenses-mfe-dev/assets/ExpensesPage-CCuebvfZ.js
@@ -0,0 +1,10 @@
+import { j as jsxRuntimeExports } from "./jsx-runtime-TULtkvNU.js";
+import { E as ExpensesList } from "./ExpensesList-BdaM3NF2.js";
+import "./axiosInstance-4KTxT63W.js";
+import "./expense-api-CbxhUX1a.js";
+const ExpensesPage = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(ExpensesList, {});
+};
+export {
+  ExpensesPage as default
+};
